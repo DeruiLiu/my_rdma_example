d3d38ce2db34d192e5244d34857fe7a240040b1b
derui <32285152+DeruiLiu@users.noreply.github.com>
2020-10-22 17:05:32 +0800

    Update setup_ib.h

--- rc-send-receive/setup_ib.h
+++ rc-send-receive/setup_ib.h
@@ -1,26 +1,30 @@
 #ifndef SETUP_IB_H
 #define SETUP_IB_H
 
-#include<infiniband/verbs.h>
+#include <infiniband/verbs.h>
 
-
-struct ibcontext{
-    struct ibv_context  *context;
-	struct ibv_comp_channel *channel;
-    struct ibv_pd       *pd;
-    struct ibv_mr       *mr;
-    struct ibv_cq       *cq;
-    struct ibv_qp       *qp;
-    struct ibv_port_attr    port_info;
-	int 				pending;
-	int rx_depth;
-
-    void                    *buf;
-    unsigned long long      size;
+enum {
+    IB_RECV_WRID = 1,
+    IB_SEND_WRID = 2,
 };
 
 
-struct ibdest{
+struct ibcontext {
+    struct ibv_context *context;
+    struct ibv_comp_channel *channel;
+    struct ibv_pd *pd;
+    struct ibv_mr *mr;
+    struct ibv_cq *cq;
+    struct ibv_qp *qp;
+    struct ibv_port_attr port_info;
+    int pending;
+    int rx_depth;
+
+    void *buf;
+    unsigned long long size;
+};
+
+struct ibdest {
     int lid;
     int qpn;
     int psn;
@@ -33,11 +37,13 @@ int ib_post_recv(struct ibcontext *ctx, int n);
 struct ibdest *ib_server_exch_dest(int port, const struct ibdest *my_dest);
 struct ibdest *ib_client_exch_dest(const char *servername, int port, const struct ibdest *my_dest);
 void usage(const char *argv0);
-int connect_between_qps(struct ibcontext *ctx, int port, int my_psn, enum ibv_mtu mtu, int sl,struct ibdest *dest, int sgid_idx);
-int post_send(struct ibcontext * ctx);
+int connect_between_qps(struct ibcontext *ctx, int port, int my_psn, enum ibv_mtu mtu, int sl, struct ibdest *dest,
+    int sgid_idx);
+int post_send(struct ibcontext *ctx);
 //
 int ib_close_ctx(struct ibcontext *ctx);
 void wire_gid_to_gid(const char *wgid, union ibv_gid *gid);
 void gid_to_wire_gid(const union ibv_gid *gid, char wgid[]);
+enum ibv_mtu ib_mtu_to_enum(int mtu);
 
 #endif
