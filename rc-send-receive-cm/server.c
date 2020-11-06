d1751d9814e3dd3c805e9a2b3f65843f72b3e51f
derui <32285152+DeruiLiu@users.noreply.github.com>
2020-11-06 11:01:31 +0800

    Create server.c

---
+++ rc-send-receive-cm/server.c
@@ -0,0 +1,291 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <rdma/rdma_cma.h>
+
+#define TEST_NZ(x) do { if ( (x)) die("error: " #x " failed (returned non-zero)." ); } while (0)
+#define TEST_Z(x)  do { if (!(x)) die("error: " #x " failed (returned zero/null)."); } while (0)
+
+const int BUFFER_SIZE = 1024;
+
+struct context {
+  struct ibv_context *ctx;
+  struct ibv_pd *pd;
+  struct ibv_cq *cq;
+  struct ibv_comp_channel *comp_channel;
+
+  pthread_t cq_poller_thread;
+};
+
+struct connection {
+  struct ibv_qp *qp;
+
+  struct ibv_mr *recv_mr;
+  struct ibv_mr *send_mr;
+
+  char *recv_region;
+  char *send_region;
+};
+
+static void die(const char *reason);
+
+static void build_context(struct ibv_context *verbs);
+static void build_qp_attr(struct ibv_qp_init_attr *qp_attr);
+static void * poll_cq(void *);
+static void post_receives(struct connection *conn);
+static void register_memory(struct connection *conn);
+
+static void on_completion(struct ibv_wc *wc);
+static int on_connect_request(struct rdma_cm_id *id);
+static int on_connection(void *context);
+static int on_disconnect(struct rdma_cm_id *id);
+static int on_event(struct rdma_cm_event *event);
+
+static struct context *s_ctx = NULL;
+
+int main(int argc, char **argv)
+{
+#if _USE_IPV6
+  struct sockaddr_in6 addr;
+#else
+  struct sockaddr_in addr;
+#endif
+  struct rdma_cm_event *event = NULL;
+  struct rdma_cm_id *listener = NULL;
+  struct rdma_event_channel *ec = NULL;
+  uint16_t port = 0;
+
+  memset(&addr, 0, sizeof(addr));
+#if _USE_IPV6
+  addr.sin6_family = AF_INET6;
+#else
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(20079);
+#endif
+
+
+  TEST_Z(ec = rdma_create_event_channel());//打开一个用于报告通信事件的事件通道，异步事件通过事件通道报告给用户
+  TEST_NZ(rdma_create_id(ec, &listener, NULL, RDMA_PS_TCP));//创建用于跟踪通信信息的标识符，在概念上类似于套接字
+  TEST_NZ(rdma_bind_addr(listener, (struct sockaddr *)&addr));//将源地址与rdma_cm_id关联起来，
+  TEST_NZ(rdma_listen(listener, 10)); //启动侦听传入的连接请求或数据报服务查找，监听被限制到本地绑定的源地址
+
+
+  //network to host short
+  port = ntohs(rdma_get_src_port(listener));//检索已绑定到本地地址的rdma_cm_id的本地端口号，如果id没有绑定到端口，例程将返回0
+
+  printf("listening on port %d.\n", port);
+
+  while (rdma_get_cm_event(ec, &event) == 0) {
+    struct rdma_cm_event event_copy;
+
+    memcpy(&event_copy, event, sizeof(*event));
+    rdma_ack_cm_event(event);
+
+    if (on_event(&event_copy))
+      break;
+  }
+
+  rdma_destroy_id(listener);
+  rdma_destroy_event_channel(ec);
+
+  return 0;
+}
+
+void die(const char *reason)
+{
+  fprintf(stderr, "%s\n", reason);
+  exit(EXIT_FAILURE);
+}
+
+void build_context(struct ibv_context *verbs)
+{
+  if (s_ctx) {
+    if (s_ctx->ctx != verbs)
+      die("cannot handle events in more than one context.");
+
+    return;
+  }
+
+  s_ctx = (struct context *)malloc(sizeof(struct context));
+
+  s_ctx->ctx = verbs;
+
+  TEST_Z(s_ctx->pd = ibv_alloc_pd(s_ctx->ctx));
+  TEST_Z(s_ctx->comp_channel = ibv_create_comp_channel(s_ctx->ctx));
+  TEST_Z(s_ctx->cq = ibv_create_cq(s_ctx->ctx, 10, NULL, s_ctx->comp_channel, 0)); /* cqe=10 is arbitrary */
+  TEST_NZ(ibv_req_notify_cq(s_ctx->cq, 0));
+
+  TEST_NZ(pthread_create(&s_ctx->cq_poller_thread, NULL, poll_cq, NULL));
+}
+
+void build_qp_attr(struct ibv_qp_init_attr *qp_attr)
+{
+  memset(qp_attr, 0, sizeof(*qp_attr));
+
+  qp_attr->send_cq = s_ctx->cq;
+  qp_attr->recv_cq = s_ctx->cq;
+  qp_attr->qp_type = IBV_QPT_RC;
+
+  qp_attr->cap.max_send_wr = 10;
+  qp_attr->cap.max_recv_wr = 10;
+  qp_attr->cap.max_send_sge = 1;
+  qp_attr->cap.max_recv_sge = 1;
+}
+
+void * poll_cq(void *ctx)
+{
+  struct ibv_cq *cq;
+  struct ibv_wc wc;
+
+  while (1) {
+    TEST_NZ(ibv_get_cq_event(s_ctx->comp_channel, &cq, &ctx));
+    ibv_ack_cq_events(cq, 1);
+    TEST_NZ(ibv_req_notify_cq(cq, 0));
+
+    while (ibv_poll_cq(cq, 1, &wc))
+      on_completion(&wc);
+  }
+
+  return NULL;
+}
+
+void post_receives(struct connection *conn)
+{
+  struct ibv_recv_wr wr, *bad_wr = NULL;
+  struct ibv_sge sge;
+
+  wr.wr_id = (uintptr_t)conn;
+  wr.next = NULL;
+  wr.sg_list = &sge;
+  wr.num_sge = 1;
+
+  sge.addr = (uintptr_t)conn->recv_region;
+  sge.length = BUFFER_SIZE;
+  sge.lkey = conn->recv_mr->lkey;
+
+  TEST_NZ(ibv_post_recv(conn->qp, &wr, &bad_wr));
+}
+
+void register_memory(struct connection *conn)
+{
+  conn->send_region = malloc(BUFFER_SIZE);
+  conn->recv_region = malloc(BUFFER_SIZE);
+
+  TEST_Z(conn->send_mr = ibv_reg_mr(
+    s_ctx->pd,
+    conn->send_region,
+    BUFFER_SIZE,
+    0));
+
+  TEST_Z(conn->recv_mr = ibv_reg_mr(
+    s_ctx->pd,
+    conn->recv_region,
+    BUFFER_SIZE,
+    IBV_ACCESS_LOCAL_WRITE));
+}
+
+void on_completion(struct ibv_wc *wc)
+{
+  if (wc->status != IBV_WC_SUCCESS)
+    die("on_completion: status is not IBV_WC_SUCCESS.");
+
+  if (wc->opcode & IBV_WC_RECV) {
+    struct connection *conn = (struct connection *)(uintptr_t)wc->wr_id;
+
+    printf("received message: %s\n", conn->recv_region);
+
+  } else if (wc->opcode == IBV_WC_SEND) {
+    printf("send completed successfully.\n");
+  }
+}
+
+int on_connect_request(struct rdma_cm_id *id)
+{
+  struct ibv_qp_init_attr qp_attr;
+  struct rdma_conn_param cm_params;
+  struct connection *conn;
+
+  printf("received connection request.\n");
+
+  build_context(id->verbs);//创建除QP内存以外的其他资源
+  build_qp_attr(&qp_attr);//qp属性
+
+  TEST_NZ(rdma_create_qp(id, s_ctx->pd, &qp_attr));//此时的QP为init
+
+  id->context = conn = (struct connection *)malloc(sizeof(struct connection));
+  conn->qp = id->qp;
+
+  register_memory(conn);//注册内存
+  post_receives(conn);//此处调用post_receive就是为了接下来能够通过cm收到对端的信息
+
+  memset(&cm_params, 0, sizeof(cm_params));
+  TEST_NZ(rdma_accept(id, &cm_params));//在侦听端调用rdma_accept来接受连接或数据报服务查找请求
+  //同时qp从init转化为rtr，rts，并通过write将自己的信息发送至发送端
+
+  return 0;
+}
+
+int on_connection(void *context)
+{
+  struct connection *conn = (struct connection *)context;
+  struct ibv_send_wr wr, *bad_wr = NULL;
+  struct ibv_sge sge;
+
+  //将buffer_size大小的数据写到send_region
+  snprintf(conn->send_region, BUFFER_SIZE, "message from passive/server side with pid %d", getpid());
+
+  printf("connected. posting send...\n");
+
+  sge.addr = (uintptr_t)conn->send_region;
+  sge.length = BUFFER_SIZE;
+  sge.lkey = conn->send_mr->lkey;
+  memset(&wr, 0, sizeof(wr));
+
+  wr.opcode = IBV_WR_SEND;
+  wr.sg_list = &sge;
+  wr.num_sge = 1;
+  wr.send_flags = IBV_SEND_SIGNALED;
+
+  TEST_NZ(ibv_post_send(conn->qp, &wr, &bad_wr));
+
+  return 0;
+}
+
+int on_disconnect(struct rdma_cm_id *id)
+{
+  struct connection *conn = (struct connection *)id->context;
+
+  printf("peer disconnected.\n");
+
+  rdma_destroy_qp(id);
+
+  ibv_dereg_mr(conn->send_mr);
+  ibv_dereg_mr(conn->recv_mr);
+
+  free(conn->send_region);
+  free(conn->recv_region);
+
+  free(conn);
+
+  rdma_destroy_id(id);
+
+  return 0;
+}
+
+int on_event(struct rdma_cm_event *event)
+{
+  int r = 0;
+
+  if (event->event == RDMA_CM_EVENT_CONNECT_REQUEST)//在被动端生成，用于通知用户新的连接请求
+    r = on_connect_request(event->id);
+  else if (event->event == RDMA_CM_EVENT_ESTABLISHED)//指示已与远程端点建立连接
+    r = on_connection(event->id->context);
+  else if (event->event == RDMA_CM_EVENT_DISCONNECTED)
+    r = on_disconnect(event->id);
+  else
+    die("on_event: unknown event.");
+
+  return r;
+}
+
